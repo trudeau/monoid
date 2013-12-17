51897d04b65f014fcbdff37fba055b996aa3d286
Simo Tripodi <stripodi@adobe.com>
2013-12-17 15:52:47 +0100

    Wagon: Deploying apidocs to repository

---
+++ 0.0.1/assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.trudeau.math.monoid", "org/nnsoft/trudeau/math/monoid/package-summary.html", [ [ "Description", "org/nnsoft/trudeau/math/monoid/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "Monoid", "org/nnsoft/trudeau/math/monoid/Monoid.html", null, "" ], [ "OrderedMonoid", "org/nnsoft/trudeau/math/monoid/OrderedMonoid.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.nnsoft.trudeau.math.monoid.primitive", "org/nnsoft/trudeau/math/monoid/primitive/package-summary.html", [ [ "Description", "org/nnsoft/trudeau/math/monoid/primitive/package-descr.html", null, "" ], [ "Classes", null, [ [ "BigDecimalWeightBaseOperations", "org/nnsoft/trudeau/math/monoid/primitive/BigDecimalWeightBaseOperations.html", null, "" ], [ "BigIntegerWeightBaseOperations", "org/nnsoft/trudeau/math/monoid/primitive/BigIntegerWeightBaseOperations.html", null, "" ], [ "DoubleWeightBaseOperations", "org/nnsoft/trudeau/math/monoid/primitive/DoubleWeightBaseOperations.html", null, "" ], [ "FloatWeightBaseOperations", "org/nnsoft/trudeau/math/monoid/primitive/FloatWeightBaseOperations.html", null, "" ], [ "IntegerWeightBaseOperations", "org/nnsoft/trudeau/math/monoid/primitive/IntegerWeightBaseOperations.html", null, "" ], [ "LongWeightBaseOperations", "org/nnsoft/trudeau/math/monoid/primitive/LongWeightBaseOperations.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ 0.0.1/reference/lists.js
@@ -0,0 +1,13 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.trudeau.math.monoid", link:"org/nnsoft/trudeau/math/monoid/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.trudeau.math.monoid.Monoid", link:"org/nnsoft/trudeau/math/monoid/Monoid.html", type:"class" },
+      { id:2, label:"org.nnsoft.trudeau.math.monoid.OrderedMonoid", link:"org/nnsoft/trudeau/math/monoid/OrderedMonoid.html", type:"class" },
+      { id:3, label:"org.nnsoft.trudeau.math.monoid.primitive", link:"org/nnsoft/trudeau/math/monoid/primitive/package-summary.html", type:"package" },
+      { id:4, label:"org.nnsoft.trudeau.math.monoid.primitive.BigDecimalWeightBaseOperations", link:"org/nnsoft/trudeau/math/monoid/primitive/BigDecimalWeightBaseOperations.html", type:"class" },
+      { id:5, label:"org.nnsoft.trudeau.math.monoid.primitive.BigIntegerWeightBaseOperations", link:"org/nnsoft/trudeau/math/monoid/primitive/BigIntegerWeightBaseOperations.html", type:"class" },
+      { id:6, label:"org.nnsoft.trudeau.math.monoid.primitive.DoubleWeightBaseOperations", link:"org/nnsoft/trudeau/math/monoid/primitive/DoubleWeightBaseOperations.html", type:"class" },
+      { id:7, label:"org.nnsoft.trudeau.math.monoid.primitive.FloatWeightBaseOperations", link:"org/nnsoft/trudeau/math/monoid/primitive/FloatWeightBaseOperations.html", type:"class" },
+      { id:8, label:"org.nnsoft.trudeau.math.monoid.primitive.IntegerWeightBaseOperations", link:"org/nnsoft/trudeau/math/monoid/primitive/IntegerWeightBaseOperations.html", type:"class" },
+      { id:9, label:"org.nnsoft.trudeau.math.monoid.primitive.LongWeightBaseOperations", link:"org/nnsoft/trudeau/math/monoid/primitive/LongWeightBaseOperations.html", type:"class" }
+
+    ];
